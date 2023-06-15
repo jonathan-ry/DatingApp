c0e4686678df0472071e3dade02951127e6c6c88
jonathan-ry <rj.rubiano@gmail.com>
2023-06-15 21:47:24 +0800

    Add feature to save changes to DB and store state in Angular

--- API/Program.cs
+++ API/Program.cs
@@ -19,7 +19,7 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyHeader()
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
